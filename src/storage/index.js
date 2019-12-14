689d390e9171df07c2260294645bbde62cc714cf
nonoakij <matsumoto_aki@cam-inc.co.jp>
2019-12-15 03:15:02 +0900

    星の色を無理やり変更する

--- src/storage/index.js
+++ src/storage/index.js
@@ -2,6 +2,7 @@ import firebase from 'firebase/app';
 import 'firebase/firestore';
 import firebaseConfig from '../../env';
 
+
 firebase.initializeApp(firebaseConfig);
 const db = firebase.firestore();
 
