9bec7a35342a23aa9dfdfc6fac72045cb3e96b45
Daiki <ruman.orchid@gmail.com>
2019-12-14 18:21:46 +0900

    Update: entry を appに分けましたー

--- src/app/index.js
+++ src/app/index.js
@@ -1,16 +1,1 @@
-import { Router } from '@vaadin/router';
-
-window.addEventListener('load', () => {
-  initRouter();
-})
-
-function initRouter () {
-  const router = new Router(document.getElementById('outlet'));
-  router.setRoutes([
-    {
-    path: '/',
-    component: 'a100-page-home',
-    action: () => import('../pages/home/index.ts')
-  }
-  ])
-}
+import '../router';
